238aa035f713719103e772c35f82c48e5a10a188
computerlngako <Ambajolo700@gmail.com>
2025-09-27 13:52:34 +0800

    feat: US002 - Define IAuthService interface

---
+++ StudentDiary.Services/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StudentDiary.Services.DTOs;
+
+namespace StudentDiary.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        bool Register(UserRegisterDTO dto);
+        bool Login(UserLoginDTO dto);
+    }
+}
