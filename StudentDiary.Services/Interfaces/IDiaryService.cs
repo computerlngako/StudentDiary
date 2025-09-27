8f0b20f98d798bbb40179439a52314364eae538f
computerlngako <Ambajolo700@gmail.com>
2025-09-27 13:55:44 +0800

    feat: US002 - Define IDiaryService interface

---
+++ StudentDiary.Services/Interfaces/IDiaryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StudentDiary.Services.DTOs;
+
+namespace StudentDiary.Services.Interfaces
+{
+    public interface IDiaryService
+    {
+        void CreateEntry(DiaryEntryCreateDTO dto);
+        void UpdateEntry(DiaryEntryUpdateDTO dto);
+        void DeleteEntry(int id);
+        DiaryEntryReadDTO GetEntry(int id);
+        IEnumerable<DiaryEntryReadDTO> GetAllEntries();
+    }
+}
+
