9a1acc83e6583dd6f7208707bcfa48b513e690e1
computerlngako <Ambajolo700@gmail.com>
2025-09-27 13:47:26 +0800

    feat: US002 - Add DTOs for diary CRUD operations

---
+++ StudentDiary.Services/DTOs/DiaryEntryDTOs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary.Services.DTOs
+{
+    public class DiaryEntryCreateDTO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class DiaryEntryUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class DiaryEntryReadDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
